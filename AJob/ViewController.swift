504c3d170abf0263ece953a0495e60b208fc9201
LisetteAntigua <antigua223@gmail.com>
2022-02-10 12:15:32 -0500

    Initial Commit

---
+++ AJob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AJob
+//
+//  Created by Lisette Antigua on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
