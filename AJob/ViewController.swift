fd86126c814154cb961e410c1b60bfc792580452
LisetteAntigua <antigua223@gmail.com>
2022-04-07 10:46:35 -0400

    Update Firebase

--- AJob/ViewController.swift
+++ AJob/ViewController.swift
@@ -6,15 +6,31 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signOut: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signOut(_ sender: Any) {
+       
+        // 1
+        guard let user = Auth.auth().currentUser else { return }
+       
+          // 4
+          do {
+            try Auth.auth().signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+          } catch let error {
+            print("Auth sign out failed: \(error)")
+          }
+        }
+
+  
 }
 
  
